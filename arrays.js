e0a1d1ad858d30404ff80f26f8d0753d87b80178
Chen Ye <dpp0921@gmail.com>
2017-12-28 04:21:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -24,7 +24,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 function removeElementFromBeginningOfArray(array){
   array.slice(1);
-  return array;
+  array;
 }
 function destructivelyRemoveElementFromEndOfArray(array){
   array.pop();
