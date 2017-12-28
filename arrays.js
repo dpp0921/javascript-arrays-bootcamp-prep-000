844a223121b5ea33e53ea6769e9306010d924898
Chen Ye <dpp0921@gmail.com>
2017-12-28 02:26:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,11 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element){
-  addElementToBeginningOfArray.push();
-  return;
+  array.unshift(element);
+  return array;
 }
-function destructivelyAddElementToBeginningOfArray(){
-  
+function destructivelyAddElementToBeginningOfArray(array, element){
+  ['element', ...array]
 }
 function addElementToEndOfArray(){
   
