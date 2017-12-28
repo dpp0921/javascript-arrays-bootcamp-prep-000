a7670729895e43189ce97de290f3e48eafbbf1f3
Chen Ye <dpp0921@gmail.com>
2017-12-28 02:27:00 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,6 +6,7 @@ function addElementToBeginningOfArray(array, element){
 }
 function destructivelyAddElementToBeginningOfArray(array, element){
   ['element', ...array]
+  return array;
 }
 function addElementToEndOfArray(){
   
