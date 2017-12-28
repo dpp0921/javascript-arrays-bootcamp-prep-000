5b88797c8972813536555a860d3ab90c93fa7467
Chen Ye <dpp0921@gmail.com>
2017-12-28 03:03:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element){
-  [element, ...array];
+  ['foo', ...array];
   return array;
 }
 function destructivelyAddElementToBeginningOfArray(array, element){
@@ -9,7 +9,7 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   return array;
 }
 function addElementToEndOfArray(array, element){
-  [...array, element];
+  [...array, 'foo'];
   return array;
 }
 function destructivelyAddElementToEndOfArray(array, element){
